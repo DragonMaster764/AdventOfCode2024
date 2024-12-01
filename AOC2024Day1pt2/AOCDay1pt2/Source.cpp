50042a1b9990d635442bfb794e00ee7318bd86c3
DragonMaster764 <tanner98072@gmail.com>
2024-12-01 12:09:02 -0500

    AOC2024Day1Solutions
    Solutions to Day One of Advent Of Code 2024

---
+++ AOC2024Day1pt2/AOCDay1pt2/Source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	// Intialize some vectors to store our inputs
+	vector<int> list1;
+	vector<int> list2;
+
+	// Import the data
+	ifstream fileRead("Input.txt", ifstream::in);
+
+	// Parse the data
+	string input;
+	while (getline(fileRead, input))
+	{
+		list1.push_back(stoi(input.substr(0, input.find_first_of(' '))));
+		list2.push_back(stoi(input.substr(input.find_last_of(' '))));
+	}
+
+	// Sort the data in ascending order
+	sort(list1.begin(), list1.end());
+	sort(list2.begin(), list2.end());
+
+
+	//Double check they are same size before doing the math
+	if (list1.size() == list2.size())
+	{
+		int simScore = 0;
+
+		for (int i = 0; i < list1.size(); i++)
+		{
+			simScore += list1[i] * count(list2.begin(), list2.end(), list1[i]);
+		}
+		cout << simScore << endl;
+	}
+	else
+	{
+		cout << "Lists aren't the same size!" << endl;
+	}
+
+
+	return 0;
+}
